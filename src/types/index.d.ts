d7a184a593b91116eecde5f274892132434c13f2
Harsh Mriduhash <Harshsahay2709@gmail.com>
2025-01-27 12:32:59 +0530

    Index

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,27 +1,27 @@
 export type NavItem = {
-  title: string
-  href: string
-  disabled?: boolean
-}
+  title: string;
+  href: string;
+  disabled?: boolean;
+};
 
-export type MainNavItem = NavItem
+export type MainNavItem = NavItem;
 
 export type MarketingConfig = {
-  mainNav: MainNavItem[]
-}
+  mainNav: MainNavItem[];
+};
 
 export type SiteConfig = {
-  name: string
-  description: string
-  url: string
-  ogImage: string
-  creator: { name: string, url: string }
-  keywords: string[]
+  name: string;
+  description: string;
+  url: string;
+  ogImage: string;
+  creator: { name: string; url: string };
+  keywords: string[];
   links: {
-    twitter: string
-    github: string
-  }
-}
+    twitter: string;
+    github: string;
+  };
+};
 
 export type gpt = {
   id: string;
