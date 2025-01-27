0a3564fbb00c77e5804b2e6ae008252ce992bb5c
Harsh Mriduhash <Harshsahay2709@gmail.com>
2025-01-27 12:06:39 +0530

    routing completed

--- src/app/api/gpt/route.ts
+++ src/app/api/gpt/route.ts
@@ -3,12 +3,12 @@ import { gptSchemaBody } from "@/lib/validations/gpt";
 
 export async function POST(req: Request) {
   try {
-    const json = await req.json()
-    const body = gptSchemaBody.parse(json)
+    const json = await req.json();
+    const body = gptSchemaBody.parse(json);
     let user = await db.user.findUnique({
       where: {
         email: body.email,
-      }
+      },
     });
 
     if (!user) {
